0c17da1047d037fe9c8ee81f09857a3da4af5077
LeandroSAlmeida <contact.dev.leandro@gmail.com>
2024-07-21 19:45:28 -0300

    feat: adding security in routes

--- src/main/java/com/devsuperior/demo/controllers/CityController.java
+++ src/main/java/com/devsuperior/demo/controllers/CityController.java
@@ -2,19 +2,17 @@ package com.devsuperior.demo.controllers;
 
 import com.devsuperior.demo.dto.CityDTO;
 import com.devsuperior.demo.services.CityService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
 import java.util.List;
 
-@Service
+@RestController
 @RequestMapping(value = "/cities")
 public class CityController {
 
@@ -23,12 +21,13 @@ public class CityController {
 
     @GetMapping
     public ResponseEntity<List<CityDTO>> findAll() {
-        List<CityDTO> list = service.findAll();
+        List<CityDTO> list = service.findAllSorted();
         return ResponseEntity.ok(list);
     }
 
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN')")
     @PostMapping
-    public ResponseEntity<CityDTO> insert(@RequestBody CityDTO dto) {
+    public ResponseEntity<CityDTO> insert(@Valid @RequestBody CityDTO dto) {
         dto = service.insert(dto);
         URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
                 .buildAndExpand(dto.getId()).toUri();
