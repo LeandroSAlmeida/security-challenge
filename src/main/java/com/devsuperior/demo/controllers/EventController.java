9466e7adafdd4207e7fe2ddbfd27b3ad590bf0a7
LeandroSAlmeida <contact.dev.leandro@gmail.com>
2024-07-21 18:07:21 -0300

    feat: create controllers, CityController and EventController

---
+++ src/main/java/com/devsuperior/demo/controllers/EventController.java
@@ -0,0 +1,38 @@
+package com.devsuperior.demo.controllers;
+
+import com.devsuperior.demo.dto.EventDTO;
+import com.devsuperior.demo.services.EventService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+
+@Service
+@RequestMapping(value = "/events")
+public class EventController {
+
+    @Autowired
+    private EventService service;
+
+    @GetMapping
+    public ResponseEntity<Page<EventDTO>> findAll(Pageable pageable) {
+        Page<EventDTO> list = service.findAll(pageable);
+        return ResponseEntity.ok().body(list);
+    }
+
+    @PostMapping
+    public ResponseEntity<EventDTO> insert(@RequestBody EventDTO dto) {
+        dto = service.insert(dto);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+                .buildAndExpand(dto.getId()).toUri();
+        return ResponseEntity.created(uri).body(dto);
+    }
+}
