44445dfa5f460a5ff89c03df2fe53777e16b540d
LeandroSAlmeida <contact.dev.leandro@gmail.com>
2024-07-21 17:50:35 -0300

    feat: create services, CityService and EventService

---
+++ src/main/java/com/devsuperior/demo/services/CityService.java
@@ -0,0 +1,28 @@
+package com.devsuperior.demo.services;
+
+import com.devsuperior.demo.dto.CityDTO;
+import com.devsuperior.demo.entities.City;
+import com.devsuperior.demo.repositories.CityRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CityService {
+
+    @Autowired
+    private CityRepository repository;
+
+    public List<CityDTO> findAll() {
+        List<City> result = repository.findAll();
+        return result.stream().map(x -> new CityDTO(x)).toList();
+    }
+
+    public CityDTO insert(CityDTO dto) {
+        City entity = new City();
+        entity.setName(dto.getName());
+        entity = repository.save(entity);
+        return new CityDTO(entity);
+    }
+}
